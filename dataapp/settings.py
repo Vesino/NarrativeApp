cd041b0ae29460a103b7bb50ddad2f3c32c5c226
Luis Vargas <luisvargasr10@gmail.com>
2021-02-17 13:10:53 -0600

    setttings

--- dataapp/settings.py
+++ dataapp/settings.py
@@ -69,7 +69,7 @@ WSGI_APPLICATION = 'dataapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
